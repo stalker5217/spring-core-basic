c763ab70f03737d45d7d9019caa130f7f6610456
songmk <stalker5217@gmail.com>
2022-04-24 18:09:25 +0900

    스프링 전환

--- src/main/java/com/example/springcorebasic/AppConfig.java
+++ src/main/java/com/example/springcorebasic/AppConfig.java
@@ -1,5 +1,8 @@
 package com.example.springcorebasic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.example.springcorebasic.discount.DiscountPolicy;
 import com.example.springcorebasic.discount.RateDiscountPolicy;
 import com.example.springcorebasic.member.MemberService;
@@ -8,12 +11,15 @@ import com.example.springcorebasic.member.MemoryMemberRepository;
 import com.example.springcorebasic.order.OrderService;
 import com.example.springcorebasic.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
 
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(
 			memberRepository(),
@@ -21,10 +27,12 @@ public class AppConfig {
 		);
 	}
 
+	@Bean
 	public MemoryMemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public DiscountPolicy discountPolicy() {
 		return new RateDiscountPolicy();
 	}
